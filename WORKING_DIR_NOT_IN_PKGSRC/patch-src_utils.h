2a3b87ed1953681f1f028f8cd0bc2544f98f426e
Cory Tertel <ctertel@comcast.net>
2021-11-05 00:50:47 -0700

    working dir

---
+++ WORKING_DIR_NOT_IN_PKGSRC/patch-src_utils.h
@@ -0,0 +1,18 @@
+$NetBSD: patch-src_utils.h,v 1.0 2021/11/05 12:22:08 cory Exp $
+
+Conflicting type definition with libc popcountl
+
+--- ./utils.h.orig	2021-11-04 23:38:28.858700476 -0700
++++ ./utils.h	2021-11-05 00:14:40.738395801 -0700
+@@ -114,9 +114,11 @@
+ /// clamp `val` into interval [min, max]
+ #define clamp(val, min, max) max2(min2(val, max), min)
+ 
++#ifndef __NetBSD__
+ static inline int attr_const popcountl(unsigned long a) {
+ 	return __builtin_popcountl(a);
+ }
++#endif
+ 
+ /**
+  * Normalize a double value to a specific range.
